3656b07ae4500bf3e5b8e205799a581230a87475
henriqueZapellaRocha <henri_zap@hotmail.com>
2025-07-02 15:16:18 -0300

    resolving bugs in parsing tree

--- src/parse_tree/definitions/parsing_tree.h
+++ src/parse_tree/definitions/parsing_tree.h
@@ -3,12 +3,14 @@
 
 #define NODE_TYPE_OBJECT char // -> don`t know it wil bee really necessary, adding if its will be. @todo analyze this
  
-typedef struct parsing_tree {
+typedef struct PARSING_TREE {
     char *name;
     NODE_TYPE_OBJECT node_type_object;
-    parsing_tree *childrens;
+    struct PARSING_TREE **childrens;
+    int childrens_len;
 } PARSING_TREE;
 
 PARSING_TREE *create_tree( char *name, NODE_TYPE_OBJECT node_type );
+PARSING_TREE *add_children( char *name, NODE_TYPE_OBJECT node_type, PARSING_TREE *tree );
 
 #endif 
---
+++ src/parse_tree/implementations/parsing_tree.c
@@ -0,0 +1,27 @@
+#include "../definitions/parsing_tree.h"
+#include <stdlib.h>
+
+PARSING_TREE *create_tree( char *name, NODE_TYPE_OBJECT node_type ) {
+    
+    PARSING_TREE *parsing_tree = malloc(sizeof(PARSING_TREE));
+    parsing_tree->name = name;
+    parsing_tree->node_type_object = node_type;
+    parsing_tree->childrens = NULL;
+    parsing_tree->childrens_len = -1;
+
+    return parsing_tree;
+}
+
+PARSING_TREE *add_children( char *name, NODE_TYPE_OBJECT node_type, PARSING_TREE *tree ) {
+
+    PARSING_TREE *new_children = malloc(sizeof(PARSING_TREE));
+    new_children->name = name;
+    new_children->childrens_len = 0;
+    new_children->node_type_object = node_type;
+
+    tree->childrens_len++;
+    tree->childrens = realloc(tree->childrens, sizeof(PARSING_TREE*)*tree->childrens_len);
+    tree->childrens[tree->childrens_len] = new_children;
+
+    return new_children;
+}
